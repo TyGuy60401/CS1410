cefc1610c6e1ecb4bbee1bbc173cf125dc5f9a18
Ty Davis <tydavis.cougarfan@gmail.com>
2024-05-18 15:34:05 -0600

    Scratch work

--- scratch_work/zy_labs_3_27.cpp
+++ scratch_work/zy_labs_3_27.cpp
@@ -3,27 +3,27 @@
 using namespace std;
 
 int main() {
-   string login;
-   string first;
-   string last;
-   int number;
-   string numberString;
-   int firstSlice;
+    string login;
+    string first;
+    string last;
+    int number;
+    string numberString;
+    int firstSlice;
 
 
-   cin >> first;
-   cin >> last;
-   cin >> number;
+    cin >> first;
+    cin >> last;
+    cin >> number;
 
-   firstSlice = (first.length() >= 6) ? 6 : first.length();
+    firstSlice = (first.length() >= 6) ? 6 : first.length();
 
-   numberString = to_string(number);
+    numberString = to_string(number);
 
-   login = first.substr(0, firstSlice) + last.at(0) + "_" + numberString.at(numberString.length() - 1);
+    login = first.substr(0, firstSlice) + last.at(0) + "_" + numberString.at(numberString.length() - 1);
 
-   cout << "Your login name: ";
-   cout << login << endl;
+    cout << "Your login name: ";
+    cout << login << endl;
 
 
-   return 0;
+    return 0;
 }  
