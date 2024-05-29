e1a0df0c996c675f6f2ae0cb6da9b4f101a2ec27
Ty Davis <tdavis@barnesaero.com>
2024-05-29 09:23:21 -0600

    Started your turn 3 from the 4.8 video

--- module0/temp_conversion_1.py
+++ module0/temp_conversion_1.py
@@ -6,3 +6,4 @@ cel = (far - 32.0) * 5.0/9.0
 print(far, 'degrees fahrenheit is', cel, 'degrees celcius')
 
 print(f"{far:.1f} degrees fahrenheit is {cel:.1f} degrees celcius")
+
---
+++ module1/your_turn_3/main.cpp
@@ -0,0 +1,24 @@
+/*
+ * Name: Ty Davis
+ * Desc: Your turn 3
+ *     
+ *
+ * Pseudocode:
+ *
+ *  Integrity Statement:
+ *  
+ *  I certify that this assignment represent my own work.
+ *  I have not used any unauthorized or unacknoledged assistance
+ *  or sources in completing it, including free or commercial
+ *  systems or services offered on the internet or code-generating
+ *  systems embedded into software.
+*/
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+
+	return 0;
+}
