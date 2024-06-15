3b911472a2ae42b29139832794a841dae8af8f81
Ty Davis <tydavis.cougarfan@gmail.com>
2024-06-15 15:59:55 -0600

    Some changes to the gitignore

---
+++ test_cplusplus/testing_1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	double myDoubles[5] = {1.1, 2.2, 3.3, 4.4, 5.5};
+	for (int i = 0; i < 5; i++) {
+		cout << myDoubles[i] << " ";
+	}
+	cout << endl;
+}
